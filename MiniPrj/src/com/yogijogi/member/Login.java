88754d016ea6f1b6f4856f156745dfb41462e801
Johnny <skawogus5859@gmail.com>
2022-05-27 13:32:44 +0900

    마이페이지+유저 추가

--- MiniPrj/src/com/yogijogi/member/Login.java
+++ MiniPrj/src/com/yogijogi/member/Login.java
@@ -11,8 +11,9 @@ import com.yogijogi.obj.ObjController;
 import com.yogijogi.obj.OracleDB;
 
 public class Login {
+	// page.showPage(login.loginView());
 
-	public void loginView() {
+	public User loginView() {
 		//로그인 화면
 		
 		//로그인창
@@ -21,16 +22,20 @@ public class Login {
 		String id = ObjController.scanStr();
 		System.out.print("PW : ");
 		String pwd = ObjController.scanStr();
+		User user = checkUser(id, pwd);
 		
 		//가입된 유저인지 확인
-		if(checkUser(id, pwd)) {
+		if(user != null) {
+			User.LoginUserNo = user.getNo();
 			System.out.println("로그인 성공!");
 		}else {
 			System.out.println("로그인 실패...");
 		}
+		
+		return user;
 	}
 	
-	public boolean checkUser(String scanId, String scanPwd) {
+	public User checkUser(String scanId, String scanPwd) {
 		//유저 확인
 		
 		Connection conn = OracleDB.getOracleConnection();
@@ -41,14 +46,25 @@ public class Login {
 			pstmt = conn.prepareStatement("SELECT * FROM MEMBER WHERE ID = ? AND PWD = ? AND DROP_YN = 'N'");
 			pstmt.setString(1, scanId);
 			pstmt.setString(2, scanPwd);
-			rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery();	// 결과집합을 얻어서 rs 에 저장
+			// 현재 결과집합의 커서는 0번칸(==비어있는 공간)을 가리키는 상태 
+			//결과집합에서, 현재 커서가 가리키는 row 중 칼럼명이 MEM_NO 인 곳의 int 타입 데이터를 읽어옴//엥 지금은 0번row인데 .... ㅠㅠ 에러 ㅠㅠ
+			//이래서 필요합니다
 			
 			if(rs.next()) {
-					return true;
+				int memNo = rs.getInt("MEM_NO");
+				String id = rs.getString("ID");
+				String pwd = rs.getString("PWD");
+				String uname = rs.getString("UNAME");
+				String nick = rs.getString("NICK");
+				String rank = rs.getString("RANK");
+				String dropYN = rs.getString("DROP_YN");
+				User user = new User(memNo, id, pwd, uname, nick, rank, dropYN);
+				return user;
 			}
 			
 		} catch (SQLException e) {
-			System.out.println("SQL 관련 에러 !!!");
+			e.printStackTrace();
 		} finally {
 			//자원 정리
 			OracleDB.close(conn);
@@ -56,7 +72,7 @@ public class Login {
 			OracleDB.close(rs);
 		}
 		
-		return false;
+		return null;
 		
 	}
 	
