e0c767888d46a9af4d7f935c3364882ce7a2e1b3
LAPTOP-9PM7B9FO\yewon <a>
2022-05-28 23:05:34 +0900

    검색
    준비

--- MiniPrj/src/serch/Serch.java
+++ MiniPrj/src/serch/Serch.java
@@ -1,10 +1,18 @@
 package serch;
 
+import java.sql.Connection;
+
+import com.yogijogi.obj.OracleDB;
+
 public class Serch {
 
 	public void serch() {
 		
+		System.out.println("=====검색=====");
 		
+		Connection conn = OracleDB.getOracleConnection();
+		
+		Sting sql = 
 		
 	}
 	
